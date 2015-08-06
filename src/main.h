69ebbf9034a9ac2889ee2e6ea67264651b20f9b0
ALEXIUMCOIN <ALEXIUMCOINGITHUB@gmail.com>
2015-08-06 22:27:28 +0600

    change block time to 1 minute

--- src/main.h
+++ src/main.h
@@ -60,7 +60,7 @@ inline int64_t FutureDriftV1(int64_t nTime) { return nTime + 10 * 60; }
 inline int64_t FutureDriftV2(int64_t nTime) { return nTime + 15; }
 inline int64_t FutureDrift(int64_t nTime, int nHeight) { return IsProtocolV2(nHeight) ? FutureDriftV2(nTime) : FutureDriftV1(nTime); }
 
-inline unsigned int GetTargetSpacing(int nHeight) { return 60 * 10; } //10 minutes
+inline unsigned int GetTargetSpacing(int nHeight) { return 60; } //1 minute
 
 extern CScript COINBASE_FLAGS;
 extern CCriticalSection cs_main;
