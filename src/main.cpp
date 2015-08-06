331146a0113a54b06bdfdaca5e5b8d89d1baab09
ALEXIUMCOIN <ALEXIUMCOINGITHUB@gmail.com>
2015-08-06 07:54:34 +0600

    change nStakeMinAge

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ set<pair<COutPoint, unsigned int> > setStakeSeen;
 CBigNum bnProofOfStakeLimit(~uint256(0) >> 20);
 CBigNum bnProofOfStakeLimitV2(~uint256(0) >> 48);
 
-unsigned int nStakeMinAge = 8 * 60 * 60; // 8 hours
+unsigned int nStakeMinAge = 24 * 60 * 60; // 24 hours
 unsigned int nModifierInterval = 10 * 60; // time to elapse before new modifier is computed
 
 int nCoinbaseMaturity = 500;
